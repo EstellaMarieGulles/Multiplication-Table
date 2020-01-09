183cfaf5f66fc5f3f338cf7922dd51900f2b15f8
EstellaMarieGulles <59688947+EstellaMarieGulles@users.noreply.github.com>
2020-01-09 18:43:49 +0800

    Add files via upload

---
+++ Multiplication_Table/src/multiplication_table/Multiplication_Table.java
@@ -0,0 +1,19 @@
+package multiplication_table;
+
+
+public class Multiplication_Table {
+
+    
+    public static void main(String[] args) {
+       
+        for (int i=0; i<=10; i++) {
+            for (int j=0; j<=10; j++){
+                System.out.println(i+" x "+j+ " = "+i*j);
+                
+            }
+            System.out.println("_____________");
+        }
+        
+    }
+    
+}
